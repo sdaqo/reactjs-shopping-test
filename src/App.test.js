4d767068056ac91f3a4bd38356c869b635844903
sdaqo <sdaqo.dev@protonmail.com>
2022-05-01 21:05:28 +0200

    remove the test

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
